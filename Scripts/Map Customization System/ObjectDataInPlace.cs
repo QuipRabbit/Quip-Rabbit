0a66ab7b380c4cd804147605fab3a456eaf799ae
Quip Rabbit <quiprabbit@gmail.com>
2025-03-17 11:07:38 +0700

    Create ObjectDataInPlace.cs

---
+++ Scripts/Map Customization System/ObjectDataInPlace.cs
@@ -0,0 +1,14 @@
+// After Quip Rabbit token pre-sale (ICO) is successful and reaches the hard cap. We will update the latest and complete code, thank you very much.
+
+using UnityEngine;
+
+public class ObjectDataInPlace : MonoBehaviour
+{
+    public int prefabId;
+    public int GetPrefabID()
+    {
+        return prefabId;
+    }
+}
+
+// After Quip Rabbit token pre-sale (ICO) is successful and reaches the hard cap. We will update the latest and complete code, thank you very much.
