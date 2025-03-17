876353450f3f7890f8366ff762e7ebce115f2ce6
Quip Rabbit <quiprabbit@gmail.com>
2025-03-17 10:41:08 +0700

    Create CloudScriptForFriends.js

---
+++ Scripts/Cloud Script Backend/CloudScriptForFriends.js
@@ -0,0 +1,9 @@
+// After Quip Rabbit token pre-sale (ICO) is successful and reaches the hard cap. We will update the latest and complete code, thank you very much.
+
+handlers.processFriendRequest = function (args, context) {
+    server.AddFriend({
+
+    });
+};
+
+// After Quip Rabbit token pre-sale (ICO) is successful and reaches the hard cap. We will update the latest and complete code, thank you very much.
