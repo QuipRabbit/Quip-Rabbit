76ec4ed971d07e906599fb4352abec0e83947562
Quip Rabbit <quiprabbit@gmail.com>
2025-03-17 11:00:01 +0700

    Create ObjectSaveData.cs

---
+++ Scripts/Map Customization System/ObjectSaveData.cs
@@ -0,0 +1,13 @@
+// Support us by participating in Quip Rabbit pre-sale (ICO), we will share all the game source codes after the sale reaches the hard cap. Thank you very much.
+
+using UnityEngine;
+
+[System.Serializable]
+public class ObjectSaveData
+{
+    public int id;
+    public Vector3 position;
+    public Vector3 rotation;
+}
+
+// Support us by participating in Quip Rabbit pre-sale (ICO), we will share all the game source codes after the sale reaches the hard cap. Thank you very much.
