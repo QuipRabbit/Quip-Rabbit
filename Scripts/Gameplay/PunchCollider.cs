872f1b0183b9292107856e3939692cd1c729418b
Quip Rabbit <quiprabbit@gmail.com>
2025-03-25 01:43:11 +0700

    Create PunchCollider.cs

---
+++ Scripts/Gameplay/PunchCollider.cs
@@ -0,0 +1,18 @@
+// After Quip Rabbit token pre-sale (ICO) is successful and reaches the hard cap. We will update the latest and complete code, thank you very much.
+
+using UnityEngine;
+
+public class PunchCollider : MonoBehaviour
+{
+    private float punchForce = 10f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            
+        }
+    }
+}
+
+// After Quip Rabbit token pre-sale (ICO) is successful and reaches the hard cap. We will update the latest and complete code, thank you very much.
