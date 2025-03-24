76cc6cd57bebf3b8c462e6ffabb6280d9808dd22
Quip Rabbit <quiprabbit@gmail.com>
2025-03-25 01:45:55 +0700

    Create GameCameraController.cs

---
+++ Scripts/Gameplay/GameCameraController.cs
@@ -0,0 +1,24 @@
+// After Quip Rabbit token pre-sale (ICO) is successful and reaches the hard cap. We will update the latest and complete code, thank you very much.
+
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class GameCameraController : MonoBehaviour
+{
+    [SerializeField] private CinemachineCamera cinemachineCamera;
+    private CinemachineFollow _transposer;
+    private Vector3 _followOffset;
+    public float rotationSpeed = 5f;
+
+    private Vector2 _previousPosition;
+    private bool _isDragging;
+    [SerializeField] private RectTransform panelJoystickRect;
+
+    private void Start()
+    {
+       
+    }
+
+}
+
+// After Quip Rabbit token pre-sale (ICO) is successful and reaches the hard cap. We will update the latest and complete code, thank you very much.
