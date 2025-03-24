a711a8a32a37bd59a468b6f9d2580ac304f6cba6
Quip Rabbit <quiprabbit@gmail.com>
2025-03-25 01:47:18 +0700

    Create Mode2Manager.cs

---
+++ Scripts/Game Mode Manager/Mode2Manager.cs
@@ -0,0 +1,30 @@
+// Support us by participating in Quip Rabbit pre-sale (ICO), we will share all the game source codes after the sale reaches the hard cap. Thank you very much.
+
+using System.Collections;
+using UnityEngine;
+
+public class Mode2Manager : MonoBehaviour
+{
+    [SerializeField] private Canvas _canvasGameUI;
+    [SerializeField] private Animator cameraStartAnimator;
+    [SerializeField] private Camera cameraStartAnimation;
+
+    void Start()
+    {
+        cameraStartAnimator.Play("Mode2CameraStartAnimation");
+        StartCoroutine(WaitCamera());
+    }
+
+    IEnumerator WaitCamera()
+    {
+        yield return new WaitForSeconds(4f);
+        cameraStartAnimator.enabled = false;
+        cameraStartAnimator.speed = 0;
+        cameraStartAnimation.gameObject.SetActive(false);
+        cameraStartAnimation.enabled = false;
+        _canvasGameUI.gameObject.SetActive(true);
+        _canvasGameUI.enabled = true;
+    }
+}
+
+// Support us by participating in Quip Rabbit pre-sale (ICO), we will share all the game source codes after the sale reaches the hard cap. Thank you very much.
