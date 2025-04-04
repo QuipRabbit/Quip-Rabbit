d8f60aab326b962042db6e7a908c8a29d92ecbfb
Quip Rabbit <quiprabbit@gmail.com>
2025-04-04 20:39:01 +0700

    Create Mode1BotJumpBoxCollider.cs

---
+++ Scripts/Game Mode Manager/Mode1/Mode1BotJumpBoxCollider.cs
@@ -0,0 +1,39 @@
+// Support us by participating in Quip Rabbit pre-sale (ICO), we will share all the game source codes after the sale reaches the hard cap. Thank you very much.
+
+using UnityEngine;
+
+public class Mode1BotJumpBoxCollider : MonoBehaviour
+{
+    private float jumpForce = 5f;
+    private float moveSpeed = 3f;
+    public bool isBotJump;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bot"))
+        {
+            Rigidbody botRb = other.GetComponent<Rigidbody>();
+            if (botRb != null)
+            {
+                isBotJump = true;
+                botRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                Vector3 forwardDirection = other.transform.forward * moveSpeed;
+                botRb.AddForce(forwardDirection, ForceMode.Impulse);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Bot"))
+        {
+            Rigidbody botRb = other.GetComponent<Rigidbody>();
+            if (botRb != null)
+            {
+                isBotJump = false;
+            }
+        }
+    }
+}
+
+// Support us by participating in Quip Rabbit pre-sale (ICO), we will share all the game source codes after the sale reaches the hard cap. Thank you very much.
